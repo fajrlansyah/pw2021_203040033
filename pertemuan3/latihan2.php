4688ba82b729152c818e497a97c0f94a8a722ca6
fajrlansyah <easieryousay@gmail.com>
2021-02-18 20:20:26 +0700

    menyelesaikan video pertemuan 3

---
+++ pertemuan3/latihan2.php
@@ -0,0 +1,27 @@
+<?php  
+/*
+Muhammad Fajriansyah
+203040033
+https://github.com/fajrlansyah/pw2021_203040033.git
+Pertemuan 3 - 18 Februari 2021
+Mempelajari mengenai sintaks PHP
+*/
+?>
+
+<?php  
+// Pengkondisian / Percabangan
+// if else
+// if else if else
+// ternary
+// switch
+
+$x = 20;
+if( $x <20 ) {
+	echo "benar";
+} else if ( $x == 20 ) {
+	echo "bingo!";
+} else {
+	echo "salah";
+}
+
+?>
