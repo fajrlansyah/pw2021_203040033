407b0b7b8996c48354ec3f0b468dbbd8e45b1a95
fajrlansyah <easieryousay@gmail.com>
2021-04-18 23:48:29 +0700

    tugas praktikum modul 5

---
+++ praktikum/tugas modul 5/latihan5d/index.php
@@ -0,0 +1,20 @@
+<!--
+Nama : Muhammad Fajriansyah
+Nrp : 203040033
+Shift Praktikum : Rabu pukul 10.00 WIB
+-->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Latihan 5D</title>
+     <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <?php
+    include 'php/admin.php';
+    ?>
+</body>
+</html>
