d3e64c21445101cf61bb486a73a0445f1c955588
fajrlansyah <easieryousay@gmail.com>
2021-04-27 17:21:47 +0700

    tugas praktikum modul 6

---
+++ praktikum/tugas modul 6/latihan6c/php/logout.php
@@ -0,0 +1,15 @@
+<!--
+Nama : Muhammad Fajriansyah
+Nrp : 203040033
+Shift Praktikum : Rabu pukul 10.00 WIB
+-->
+
+<?php 
+session_start();
+session_destroy();
+
+setcookie('username', '', time() - 3600);
+setcookie('hash', '', time() - 3600);
+header("Location: ../index.php");
+die;
+?>
