febca93c1e151665954782a3f7d36e25f621ecfa
fajriansyah <76413676+fajrlansyah@users.noreply.github.com>
2021-05-04 12:00:47 +0700

    tugas praktikum modul 6

---
+++ praktikum/tugas modul 6/latihan6b/php/logout.php
@@ -0,0 +1,12 @@
+<!--
+Nama : Muhammad Fajriansyah
+Nrp : 203040033
+Shift Praktikum : Rabu pukul 10.00 WIB
+-->
+
+<?php  
+session_start();
+session_destroy();
+header("Location: ../index.php");
+die;
+?>
