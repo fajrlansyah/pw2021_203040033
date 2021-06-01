1eea4f8721b032303fc31001ec112b372c5e652f
fajriansyah <76413676+fajrlansyah@users.noreply.github.com>
2021-06-02 00:44:10 +0700

    Update index.php

--- pertemuan12/index.php
+++ pertemuan12/index.php
@@ -1,3 +1,12 @@
+<?php  
+/*
+Muhammad Fajriansyah
+203040033
+https://github.com/fajrlansyah/pw2021_203040033
+Pertemuan 13 - 02 Mei 2021
+Mempelajari mengenai sintaks PHP
+*/
+?>
 <?php
 require 'php/functions.php';
 $mahasiswa = query("SELECT * FROM mahasiswa")
